87aa5f8ac8f102d38ea834207301eff3bfe2a1c0
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-05-07 15:33:20 +0300

    handle edit comment

--- frontend/src/components/CommentCard.tsx
+++ frontend/src/components/CommentCard.tsx
@@ -23,7 +23,16 @@ import { Button } from "./ui/button";
 import { useIsMobile } from "@/hooks/use-mobile";
 import { auth } from "@/firebase/config";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { deleteCommentAPI } from "@/api/commentsApi";
+import { deleteCommentAPI, updateCommentAPI } from "@/api/commentsApi";
+import { Form, FormControl, FormField, FormItem } from "./ui/form";
+import { Input } from "./ui/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const validationSchema = z.object({
+  text: z.string().min(2),
+});
 
 const CommentCard = ({
   comments,
@@ -40,9 +49,17 @@ const CommentCard = ({
 }) => {
   const navigate = useNavigate();
   const [showComments, setShowComments] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
   const isMobile = useIsMobile();
   const curUser = auth.currentUser;
   const queryClient = useQueryClient();
+
+  const form = useForm<z.infer<typeof validationSchema>>({
+    resolver: zodResolver(validationSchema),
+    defaultValues: {
+      text: comment.text || "",
+    },
+  });
 
   const replies = commentReplies({
     comments: comments!,
@@ -61,8 +78,20 @@ const CommentCard = ({
       queryClient.invalidateQueries({ queryKey: ["postById", comment.postId] }),
   });
 
+  const updateMutation = useMutation({
+    mutationFn: ({ id, text }: { id: string; text: string }) =>
+      updateCommentAPI({ id, text }),
+    onSettled: () =>
+      queryClient.invalidateQueries({ queryKey: ["postById", comment.postId] }),
+  });
+
   const handleDelete = () => {
     deleteMutation.mutate({ postId: comment.postId, commentId: comment.id });
+  };
+
+  const handleUpdate = (values: z.infer<typeof validationSchema>) => {
+    updateMutation.mutate({ id: comment.id, text: values.text });
+    setIsEditing(false);
   };
 
   return (
@@ -101,7 +130,31 @@ const CommentCard = ({
           </div>
         </CardHeader>
         <CardContent>
-          <p className="mb-2.5">{comment.text}</p>
+          {isEditing ? (
+            <Form {...form}>
+              <form
+                className="flex gap-1"
+                onSubmit={form.handleSubmit(handleUpdate)}
+              >
+                <FormField
+                  control={form.control}
+                  name="text"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormControl>
+                        <Input type="text" {...field} />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <Button variant="ghost" className="self-end">
+                  Save
+                </Button>
+              </form>
+            </Form>
+          ) : (
+            <p className="mb-2.5">{comment.text}</p>
+          )}
         </CardContent>
         <CardFooter>
           <div className="w-full flex justify-between items-center text-zinc-400 text-sm">
@@ -135,7 +188,9 @@ const CommentCard = ({
                     align="end"
                     sideOffset={4}
                   >
-                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setIsEditing(true)}>
+                      Edit
+                    </DropdownMenuItem>
                     <DropdownMenuItem onClick={handleDelete}>
                       Remove
                     </DropdownMenuItem>
